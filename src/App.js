d45d4640191fa9f66bc472891014f99d7970c7c1
Beatriz Recari <beatrizrecari@gmail.com>
2023-03-06 10:56:53 -0500

    created a bucket for photo uploads

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import "./App.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoA0rYNghea0O7Cx3AmnhA63KT07sjuBA",
+  authDomain: "affirmations-front-end.firebaseapp.com",
+  projectId: "affirmations-front-end",
+  storageBucket: "affirmations-front-end.appspot.com",
+  messagingSenderId: "932785526335",
+  appId: "1:932785526335:web:a7437781c07a4668bd4a8a",
+};
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [uploadedImage, setUploadedImage] = useState();
+  const handleUpload = (e) => {
+    e.preventDefault();
+    if (!selectedFile) {
+      alert("Please select a file first!");
+      return;
+    }
+    // connect to our bucket
+    const app = initializeApp(firebaseConfig);
+    const storage = getStorage(app);
+    // create a reference to our filename in storage
+    const filename = selectedFile.name;
+    const imageRef = ref(storage, "photo/" + filename);
+    // Todd's quick cheat to get the image url
+    const url = `https://firebasestorage.googleapis.com/v0/b/affirmations-front-end.appspot.com/o/${filename}?alt=media`;
+    // now let's upload
+    uploadBytes(imageRef, selectedFile)
+      .then(() => setUploadedImage(url))
+      .catch(alert);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleUpload}>
+          <input
+            type="file"
+            accept="*/image"
+            name="photo"
+            onChange={(e) => setSelectedFile(e.currentTarget.files[0])}
+          />
+          <button type="submit">Upload</button>
+        </form>
       </header>
+
+      {uploadedImage && <img src={uploadedImage} alt="" />}
     </div>
   );
 }
